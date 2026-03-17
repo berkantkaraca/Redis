e1ce97e8f5859a8bfdabdc06a31431576385cb7f
Berkant Karaca <berkantkaracatr@gmail.com>
2026-03-17 14:01:38 +0300

    in-memory cache

---
+++ InMemory.Caching/Program.cs
@@ -0,0 +1,25 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// In-memory caching service kaydı
+builder.Services.AddMemoryCache();
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();

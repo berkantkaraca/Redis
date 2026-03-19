399d0518638f326e5a35bbfe345f57cb6c605596
Berkant Karaca <berkantkaracatr@gmail.com>
2026-03-19 15:16:37 +0300

    redis message broker

---
+++ Redis.Pub.Sub.Example/Program.cs
@@ -0,0 +1,16 @@
+﻿using StackExchange.Redis;
+
+ConnectionMultiplexer connection = await ConnectionMultiplexer.ConnectAsync("localhost:1453", options =>
+{
+    //options.Password = "your_password";
+    //options.SslHost = "localhost";
+});
+
+ISubscriber subscriber = connection.GetSubscriber();
+
+while (true)
+{
+    Console.Write("Mesaj : ");
+    string message = Console.ReadLine();
+    await subscriber.PublishAsync("mychannel", message);
+}
